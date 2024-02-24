c406d5788d9ef7ad7252666c780b41f78555d920
mmotorock <mmotorock@gmail.com>
2024-02-24 21:35:08 +0000

    Initial upload of analyse module

---
+++ analyse.py
@@ -0,0 +1,17 @@
+# Martin Meszaros
+# Calculating the lowest overall cost of running a car
+# 24/02/2024
+# V 1.0
+
+import cost_efficiency
+import pandas as pd
+
+df = pd.read_csv('Datasets/combined_carsShrunk.csv')
+
+df['calculated_values'] = df.apply(lambda row: cost_efficiency.calculate_total_cost(row['price'], 5, 10000, row['tax'], row['mpg'], 1.5), axis=1)
+
+min_row = df.loc[df['calculated_values'].idxmin()]
+
+min_attributes = min_row[['brand', 'model', 'year']]
+
+print(min_attributes)
