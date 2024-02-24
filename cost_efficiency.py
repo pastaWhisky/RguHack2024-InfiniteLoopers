12e4caa5f2025ac445497d05a042df0571d3ef4f
mmotorock <mmotorock@gmail.com>
2024-02-24 20:25:22 +0000

    Changed calculate_total_cost to add the car price as well. Now it requires an extra attribute for car price.

--- cost_efficiency.py
+++ cost_efficiency.py
@@ -3,15 +3,14 @@
 # 24/02/2024
 # V 1.0
 
-def calculate_running_cost(years,miles,taxpy,mpg,costpl):
+def calculate_total_cost(price,years,miles,taxpy,mpg,costpl):
     mpl = mpg*(1/5.546)
     costpm = costpl*mpl
     yearly_miles_cost = costpm*miles
     total_miles_cost = yearly_miles_cost*years
     tax = taxpy*years
     total_running_cost = tax + total_miles_cost
+    total_car_cost = total_running_cost + price
 
-    return total_running_cost
-
-
+    return total_car_cost
 
