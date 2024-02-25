792c74700e11104c271b1277464b2e88aee53410
Fin <112893285+pastaWhisky@users.noreply.github.com>
2024-02-25 08:44:39 +0000

    fixed

--- analyse.py
+++ analyse.py
@@ -15,31 +15,26 @@ def analyse_data(budget,years_plan,mileage_per_year):
     df = pd.read_csv('Datasets/combined_cars_with_maintenance_final_corrected.csv')
 
     # pull petrol and diesel
-    #df2 = pd.read_excel('Datasets/fuel_prices_and_conversions.xlsx')
+    df2 = pd.read_excel('Datasets/fuel_prices_and_conversions.xlsx')
 
-    #petrol_cost = df2.loc[df2['Fuel'] == 'Petrol', 'Cost'].values[0]
-    #diesel_cost = df2.loc[df2['Fuel'] == 'Diesel', 'Cost'].values[0]
-    #print(petrol_cost)
-    #print(diesel_cost)
+    petrol_cost = df2.loc[df2['Fuel'] == 'Petrol', 'Cost'].values[0]
+    diesel_cost = df2.loc[df2['Fuel'] == 'Diesel', 'Cost'].values[0]
 
-    #print(df)
 
     # Hybrid fuel will default to petrol price, only diesel has different
-    #if (df['fuelType'] == 'Diesel'): 
-    #    fuel_cost = diesel_cost
-    #else:
-    #    fuel_cost = petrol_cost
+    if (df['fuelType'].values[0] == 'Diesel'): 
+        fuel_cost = diesel_cost
+    else:
+        fuel_cost = petrol_cost
+
 
     
-
-    
-    df['calculated_values'] = df.apply(lambda row: cost_efficiency.calculate_total_cost(budget, row['price'], years_plan, mileage_per_year, row['tax'], row['mpg'], row['MaintenanceCostYearly'], 1.5), axis=1)
+    df['calculated_values'] = df.apply(lambda row: cost_efficiency.calculate_total_cost(budget, row['price'], years_plan, mileage_per_year, row['tax'], row['mpg'], row['MaintenanceCostYearly'], fuel_cost), axis=1)
     df_sorted_by_cheapest = df.sort_values(by='calculated_values', ascending=True)
 
     #min_row = df.loc[df['calculated_values'].idxmin()]
 
     min_row = df_sorted_by_cheapest.iloc[0]
-    print(min_row)
     sec_min_row = df_sorted_by_cheapest.iloc[1]
     third_min_row = df_sorted_by_cheapest.iloc[2]
     
@@ -47,9 +42,6 @@ def analyse_data(budget,years_plan,mileage_per_year):
     sec_min_attributes = sec_min_row[['brand', 'model', 'year', 'price', 'transmission', 'mileage', 'fuelType', 'tax', 'mpg', 'engineSize']]
     third_min_attributes = third_min_row[['brand', 'model', 'year', 'price', 'transmission', 'mileage', 'fuelType', 'tax', 'mpg', 'engineSize']]
     
-    print(min_attributes)
-    print(sec_min_attributes)
-    print(third_min_attributes)
     return min_attributes, sec_min_attributes, third_min_attributes
 
 #print(min_attributes)
